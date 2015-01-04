8593d4fc253ed59edd9667fb60cf6d9eb9a76152
atlantageek <atlantageek@gmail.com>
2015-01-03 23:04:39 -0500

    Some small tweaks.

--- santa_helper_postprocess.py
+++ santa_helper_postprocess.py
@@ -4,12 +4,10 @@ solution = []
 #The leftovers are at 2810 - 9410 Year 2397
 class SantasHelperSolutionNaive(SantasHelperSolution):
     def get_target(self,biggest):
-        if (biggest > 22000):
-            return 0.40
-        elif biggest > 8000:
-            return (float(biggest - 8000) / 100000.0) + 0.25
+        if biggest > 7000:
+            return (float(biggest - 7000) / 125000.0) + 0.26
         else:
-            return 0.25
+            return 0.26
     def run(self):
         target_rating = 0.41
         while len(self.toys) > 0:
@@ -25,27 +23,21 @@ class SantasHelperSolutionNaive(SantasHelperSolution):
             target_rating = self.get_target(self.toys[len(self.toys) - 1].duration)
 
             if elf.rating > target_rating:
-                #if self.toys[toy_idx].duration <= 240:
-                    if (self.toys[toy_idx + 1].duration/elf.rating) < (time_left * 1.57) and self.toys[toy_idx + 1].duration > (600 * target_rating):
-                        toy_idx = toy_idx + 1
-                        toy = self.toys.pop(toy_idx)
-                        if elf.id == 1:
-                            print 'Elf {0} eff {1} toy {2} duration {3} MEDIUM2 TOY[{4}]'.format(elf.id, elf.rating, toy.id, toy.duration, len(self.toys))
-                    else:
-                        #Small optimization.  Look at all the leves that are about to finish and find where this elf's productivity is compared to the others.
-                        #If its the 4th (for example) most productive elf then get the 4th biggest toy leaving the bigger toys to the other elves.
-                        idx = self.get_elf_productivity_position(elf)
-                        toy_idx = (idx + 1) * -1
-                        toy = self.toys.pop( toy_idx)
-                       # toy = self.toys.pop()
-                        if elf.id == 1:
-                            print 'Elf {0} eff {1} toy {2} duration {3} Big TOY[{4}] {5} {6}'.format(elf.id, elf.rating, toy.id, toy.duration, len(self.toys), elf.rating, toy_idx)
-                        size=3
-                #else:
-                    #toy_idx = self.closest_toy_with_duration_idx(elf.rating * time_left * 1.01)
-                    #toy = self.toys.pop(toy_idx)
-                    #print 'Elf {0} eff {1} toy {2} duration {3} MEDIUM TOY[{4}]'.format(elf.id, elf.rating, toy.id, toy.duration, len(self.toys))
-                    #size=2
+                if (self.toys[toy_idx + 1].duration/elf.rating) < (time_left * 1.57) and self.toys[toy_idx + 1].duration > (600 * target_rating):
+                    toy_idx = toy_idx + 1
+                    toy = self.toys.pop(toy_idx)
+                    if elf.id == 1:
+                        print 'Elf {0} eff {1} toy {2} duration {3} MEDIUM2 TOY[{4}]'.format(elf.id, elf.rating, toy.id, toy.duration, len(self.toys))
+                else:
+                    #Small optimization.  Look at all the leves that are about to finish and find where this elf's productivity is compared to the others.
+                    #If its the 4th (for example) most productive elf then get the 4th biggest toy leaving the bigger toys to the other elves.
+                    idx = self.get_elf_productivity_position(elf)
+                    toy_idx = (idx + 1) * -1
+                    toy = self.toys.pop( toy_idx)
+                   # toy = self.toys.pop()
+                    if elf.id == 1:
+                        print 'Elf {0} eff {1} toy {2} duration {3} Big TOY[{4}] {5} {6}'.format(elf.id, elf.rating, toy.id, toy.duration, len(self.toys), elf.rating, toy_idx)
+                    size=3
             else:
                 if elf.rating == 0.25 and self.toys[0].duration > 1000:
                     toy_idx = (idx + 1) * -1
